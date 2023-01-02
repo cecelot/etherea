d705e37ddffedb41f8b02fd010f22585eea81f40
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:35:41 -0600

    feat: working IBM rom

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
+use log::error;
+use std::sync::{Arc, RwLock};
+use std::thread;
 use winit::{event::Event, event_loop::EventLoop};
 
 fn main() {
+    env_logger::init();
+
     let event_loop = EventLoop::new();
-    let mut intr = chip8::Interpreter::new(&event_loop);
+    let intr = Arc::new(RwLock::new(chip8::Interpreter::new(&event_loop)));
+
+    intr.write()
+        .unwrap()
+        .load_rom(include_bytes!("../roms/ibm-logo.ch8").to_vec());
+
+    let intr1 = Arc::clone(&intr);
+    thread::spawn(move || {
+        intr1.write().unwrap().run();
+    });
 
     event_loop.run(move |event, _, _| {
         if let Event::RedrawRequested(_) = event {
-            if let Err(e) = intr.render() {
-                eprintln!("error writing to screen: {:?}", e);
+            if let Err(e) = intr.write().unwrap().render() {
+                error!("Failed to render to screen: {}", e);
             }
         }
-        intr.get_window().request_redraw();
+        intr.write().unwrap().get_window().request_redraw();
     });
 }
